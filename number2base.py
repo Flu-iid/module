13b7c4803e46cc8fe102e244d72ddfe21c508f7d
Flu <57827666+Flu-iid@users.noreply.github.com>
2023-08-20 14:29:34 +0330

    added resultBar/progressBar module

--- number2base.py
+++ number2base.py
@@ -1,6 +1,15 @@
-def n2b(number: int, base: int):
-    output = ""
-    while number >= 1:
-        output += str(number % base)
-        number = number // base
-    return output[::-1]
+def n2b(n: int, b: int):
+    o = ""
+    while n >= 1:
+        o += str(n % b)
+        n = n // b
+    return o[::-1]
+
+
+def b2n(n: str):
+    base = int(n[0])
+    s = n[2:]
+    o = 0
+    for i, c in enumerate(s[::-1]):
+        o += int(c)*base**(i)
+    return o
