91ef82cd521b4195d28f94de8e85d83f97dddc5c
flu <57827666+Flu-iid@users.noreply.github.com>
2023-08-20 17:48:55 +0330

    added generator based function support to result lib

--- result.py
+++ result.py
@@ -8,14 +8,14 @@ decorator classes for showing different function statistics
 
 class ProgressBar:
     def __init__(self) -> None:
+        # add shapes and customizations here
         pass
 
     def __str__(self) -> str:
         return "ProgressBar obj"
 
     def test(self, func):
-        functools.wraps(func)
-
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             func(*args, **kwargs)
             print(func.__name__, f"{func.__name__!r}")
@@ -27,8 +27,7 @@ class ProgressBar:
         recieves a sequence from function.
         best to use for known length sequences!!!
         """
-        functools.wraps(func)
-
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 func_sequence = list(func(*args, **kwargs))
@@ -38,16 +37,73 @@ class ProgressBar:
 
             for i, e in enumerate(func_sequence):
                 col, row = os.get_terminal_size()
-                show = f"{i, e} > "
-                bar_length = col - len(show)
+                show_start = f"{i, e} > "
+                show_end = ""
+                bar_length = col - len(show_start) - len(show_end)
                 completition = int((i+1)/total*bar_length)
-                print(show, ("*"*completition).ljust(bar_length, "-"),
+                print(show_start, ("*"*completition).ljust(bar_length, "-"), show_end,
                       sep="", end="\r")
             print("Done!")
 
         return wrapper
 
-    # add function for simpl wait and seperated total length for generators
+    # add function for simple wait and seperated total length for generators
+
+    def gen_known(self, length):
+        """
+        decorator accepting generator-returned function with long known length in generator
+        but length calculated by user.
+        """
+        def gen(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                func_generator = func(*args, **kwargs)
+                loops = 0
+                while True:
+                    try:
+                        o = next(func_generator)
+                        loops += 1
+                        col, row = os.get_terminal_size()
+                        print(f"{o}"[:col*(row-2)].ljust(col*(row-2), " "))
+                        show_start = f"{loops} | {length} > "
+                        show_end = ""
+                        bar_length = col - len(show_start) - len(show_end)
+                        completition = int((loops+1)/length*bar_length)
+                        print(show_start, ("*"*completition).ljust(bar_length, "-"), show_end,
+                              sep="", end="\r")
+                    except StopIteration | EOFError:
+                        if loops > length:
+                            print("LENGTH calculated wrong")
+                        print("Done")
+                        break
+                return wrapper
+            return gen
+
+    def gen_uknown(self, func):
+        """
+        decorator accepting generator-returned function 
+        with long unknown length in generator.
+        use with caution!!!
+        """
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            func_generator = func(*args, **kwargs)
+            loops = 0
+            while True:
+                # try:
+                o = next(func_generator)
+                loops += 1
+                col, row = os.get_terminal_size()
+                print(f"{o}"[:col*(row-2)].ljust(col*(row-2), " "))
+                show_start = f"{loops} > "
+                show_end = ""
+                bar_length = col - len(show_start) - len(show_end)
+                completition = int((loops+1)/length*bar_length)
+                print(show_start, ("*"*completition).ljust(bar_length, "-"), show_end,
+                      sep="", end="\r")
+                # needs different shapes. probably from slp lib
+
+            # needs redundency
 
 
 if __name__ == "__main__":
@@ -55,7 +111,8 @@ if __name__ == "__main__":
     r = ProgressBar()
 
     @r.simple
-    def p(n):
+    def p():
+        # dummy function
         return range(n)
 
-    p(10**5)
+    p()
